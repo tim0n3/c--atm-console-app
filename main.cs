2983e849787b736eceb3149d2be2b010575229c0
Tim0n3 <>
2022-09-20 21:20:08 +0200

    using main, deprecated program.cs

--- main.cs
+++ main.cs
@@ -1,40 +1,200 @@
 using System;
-using System.Diagnostics;
 
-class BastionFirewall
+public class cardholder
 {
-    static void Main()
-    {
-        // lets say we want to run this command:    
-        //  t=$(echo 'this is a test'); echo "$t" | grep -o 'is a'
-        var output = ExecuteBashCommand(command: "t=$(echo 'this is a test'); echo \"$t\" | grep -o 'is a'");
-        var output = ExecuteBashCommand(command: "echo 'prevent smurf attacks.'; echo 1 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts; echo 0 > /proc/sys/net/ipv4/conf/all/accept_redirects; echo 'Drop source routed packets.'; echo 0 > /proc/sys/net/ipv4/conf/all/accept_source_route; echo 'prevent SYN Flood and TCP Starvation'; sysctl -w net/ipv4/tcp_syncookies=1; sysctl -w net/ipv4/tcp_timestamps=1; echo 2048 > /proc/sys/net/ipv4/tcp_max_syn_backlog; echo 3 > /proc/sys/net/ipv4/tcp_synack_retries; echo 'Address Spoofing Protection'; echo 1 > /proc/sys/net/ipv4/conf/all/rp_filter; echo 'Disable SYN Packet tracking'; sysctl -w net/netfilter/nf_conntrack_tcp_loose=0");
+    string cardNum;
+    int pin;
+    string firstName;
+    string lastName;
+    double balance;
 
-        // output the result
-        Console.WriteLine(output);
+    public cardholder(string cardNum, int pin, string firstName, string lastName, double balance)
+    {
+        this.cardNum = cardNum;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+
     }
 
-    static string ExecuteBashCommand(string command)
+    public String getNum()
     {
-        // according to: https://stackoverflow.com/a/15262019/637142
-        // thanks to this we will pass everything as one command
-        command = command.Replace("\"","\"\"");
+        return cardNum;
+    }
 
-        var proc = new Process
+    public int getPin()
+    {
+        return pin;
+    }
+
+    public String getFirstName()
+    {
+        return firstName;
+    }
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setNum(String newCardNum)
+    {
+        cardNum = newCardNum;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+
+    public void setFirstName(String newFirstName)
+    {
+        firstName = newFirstName;
+    }
+
+    public void setLastName(String newLastName)
+    {
+        lastName = newLastName;
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+
+    public static void Main(String[] args)
+    {
+        void printOptions()
         {
-            StartInfo = new ProcessStartInfo
+            Console.WriteLine("Please, choose from one of the following options...");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to deposit: ");
+            double deposit = Double.Parse(Console.ReadLine());
+            currentUser.setBalance(deposit);
+            Console.WriteLine("Thank you for you $$. Your new balance is: " + currentUser.getBalance());
+        }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much $$ would you like to withdraw: ");
+            double withdrawl = Double.Parse(console.ReadLine());
+            // Check if the user has enough $$$ 
+            if (currentUser.getBalance() < withdrawl)
             {
-                FileName = "/bin/bash",
-                Arguments = "-c \""+ command + "\"",
-                UseShellExecute = false,
-                RedirectStandardOutput = true,
-                CreateNoWindow = true
+                Console.WriteLine("Insufficient balance :(");
             }
-        };
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdrawl);
+                Console.WriteLine("You're good to go! Thank you :)");
+            }
+            currentUser.getBalance(withdraw);
+            Console.WriteLine("1. Deposit" + currentUser.getBalance);
+        }
 
-        proc.Start();
-        proc.WaitForExit();
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine("Current balance: " + currentUser.getBalance());
+        }
 
-        return proc.StandardOutput.ReadToEnd();
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolders.Add(new cardHolder("123456789876544321", 1234, "John", "Doe", 13579.00));
+        cardHolders.Add(new cardHolder("198765443212345678", 1987, "Jane", "Doe", 3579.00));
+        cardHolders.Add(new cardHolder("123444321567898765", 3215, "Son", "Doe", 1357.00));
+        cardHolders.Add(new cardHolder("898123456776544321", 8981, "Dun", "Doe", 1579.00));
+
+        // Promt the user
+        Console.WriteLine("Welcome to SimpleATM!");
+        Console.WriteLine("Please, insert your Debit card: ");
+        String debitCardNum = "";
+        cardHolder currentUser;
+
+        while (true)
+        {
+            debitCardNum = Console.ReadLine();
+            // Check against our DB
+            currentUser = cardHolders.FirstOrDefault(a => a.cardNum == debitCardNum);
+            if (currentUser = null)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Card is not recognized. Please, try again");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Card is not recognized. Please, try again");
+        }
+
+        Console.WriteLine("Please, enter your Pin: ";)
+        int userPin = 0;
+
+        while (true)
+        {
+            userPin = int.Parse(Console.ReadLine());
+            if (currentUser.getPin() = userPin)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Incorrect pin. Please, try again");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Incorrect pin. Please, try again");
+        }
+
+        Console.WriteLine("Welcome " + currentUser.getFirstName() + " :)");
+        int option = 0;
+        do
+        {
+            printOptions();
+            try
+            {
+                option = int.Parse(Console.ReadLine());
+            }
+            catch
+            {
+                if (option == 1)
+                {
+                    deposit(currentUser);
+                }
+                else if (option == 2)
+                {
+                    withdraw(currentUser, depositCardNum);
+                }
+                else if (option == 3)
+                {
+                    balance(currentUser);
+                }
+                else if (option == 4)
+                {
+                    break;
+                }
+                else
+                {
+                    option = 0;
+                }
+            }
+        }
+        while (option != 4);
+        Console.WriteLine("Thank you! Have a nice Day :)");
     }
 }
